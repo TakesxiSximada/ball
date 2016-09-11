88fc4602057b717d15acd6f1a44a8bfb2b577d86
TakesxiSximada <takesxi.sximada@gmail.com>
2016-09-11 18:49:07 +0900

    add menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,39 @@
-class Menu {
-    constructor(selector) {
-        this.selector = selector;
+class HeaderMenu {
+    constructor(document, visible){
+        this.document = document || window.document;
+        this.button = null;;
+        this.menu = null;
+        this.visible = visible || 'visible';
     }
+
+    toggle(event) {
+        this.menu.classList.toggle(this.visible);
+    }
+
+    loadElements(button, menu) {
+        if (typeof(button) == 'string') {
+            this.button = this.document.querySelector(button)
+        }else{
+            this.button = button;
+        }
+
+        if (typeof(menu) == 'string') {
+            this.menu = this.document.querySelector(menu)
+        }else{
+            this.menu = menu;
+        }
+    }
+
+    mount(button, menu) {
+        var self = this;
+
+        this.loadElements(button, menu);
+        this.button.addEventListener('click', (event) => {
+            self.toggle()
+        }, false, false);
+    }
+
 }
 
-console.log('AAAAAAA');
+var headerMenu = new HeaderMenu();
+headerMenu.mount('.MenuButton', '.Menu');
