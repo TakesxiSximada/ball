10662c921a66a2f6fc27575bffd616e755750716
TakesxiSximada <takesxi.sximada@gmail.com>
2016-09-11 19:15:07 +0900

    cleanup HeaderMenu

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,10 @@
-class HeaderMenu {
-    constructor(document, visible){
-        this.document = document || window.document;
-        this.button = null;;
-        this.menu = null;
-        this.visible = visible || 'visible';
-    }
+import { HeaderMenu } from './menu/index.js';
 
-    toggle(event) {
-        this.menu.classList.toggle(this.visible);
-    }
+console.log(HeaderMenu);
 
-    loadElements(button, menu) {
-        if (typeof(button) == 'string') {
-            this.button = this.document.querySelector(button)
-        }else{
-            this.button = button;
-        }
-
-        if (typeof(menu) == 'string') {
-            this.menu = this.document.querySelector(menu)
-        }else{
-            this.menu = menu;
-        }
-    }
-
-    mount(button, menu) {
-        var self = this;
-
-        this.loadElements(button, menu);
-        this.button.addEventListener('click', (event) => {
-            self.toggle()
-        }, false, false);
-    }
-
+function main(){
+    var headerMenu = new HeaderMenu();
+    headerMenu.mount('.MenuButton', '.Menu');
 }
 
-var headerMenu = new HeaderMenu();
-headerMenu.mount('.MenuButton', '.Menu');
+main();
---
+++ src/js/menu/index.js
@@ -0,0 +1,39 @@
+export class HeaderMenu {
+    constructor(document, visible){
+        this.document = document || window.document;
+        this.button = null;;
+        this.menu = null;
+        this.visible = visible || 'visible';
+    }
+
+    toggle(event) {
+        this.menu.classList.toggle(this.visible);
+    }
+
+    loadElements(button, menu) {
+        if (typeof(button) == 'string') {
+            this.button = this.document.querySelector(button)
+        }else{
+            this.button = button;
+        }
+
+        if (typeof(menu) == 'string') {
+            this.menu = this.document.querySelector(menu)
+        }else{
+            this.menu = menu;
+        }
+    }
+
+    mount(button, menu) {
+        var self = this;
+
+        this.loadElements(button, menu);
+        this.button.addEventListener('click', (event) => {
+            self.toggle()
+        }, false, false);
+    }
+}
+
+// export default function test(){
+//     console.log('OK');
+// }
